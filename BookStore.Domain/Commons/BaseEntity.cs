8eb7ba9b7d200d4a3b53830fc9ce5c764be1071b
Reza Taghavi <r.taghavi1380@gmail.com>
2023-05-05 04:02:47 +0330

    added base entity

---
+++ BookStore.Domain/Commons/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Domain.Commons
+{
+    public class BaseEntity
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+    }
+}
---
+++ BookStore.Domain/Entities/Book.cs
@@ -0,0 +1,9 @@
+﻿using BookStore.Domain.Commons;
+
+namespace BookStore.Domain.Entities
+{
+    public class Book : BaseEntity
+    {
+        public string? Name { get; set; }
+    }
+}
