b1ef4ff5787d1d0cc90bb196cd88b2b3139a261e
Reza Taghavi <r.taghavi1380@gmail.com>
2023-07-26 13:14:38 +0330

    come back to project:)

--- BookStore.Saas/BookStore.Saas.Presentation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BookStore.Saas.Presentation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
